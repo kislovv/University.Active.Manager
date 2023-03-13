0789ffecf0f0d93b24213c1562042ba71ec4e762
Kislov Kirill Alexseevich <kislov-k@inbox.ru>
2023-03-13 22:32:19 +0300

    Добавил комментарии класса Program

--- University.Active.Manager.Web/Program.cs
+++ University.Active.Manager.Web/Program.cs
@@ -7,21 +7,30 @@ using University.Active.Manager.Entity;
 using University.Active.Manager.Services;
 using University.Active.Manager.Storage;
 using University.Active.Manager.Web.Configuration;
-using EventFakeRepository = University.Active.Manager.Storage.EventFakeRepository;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Добавление БД (метод расширения в проекте Storage)
 builder.Services.AddDatabase();
 
+//Поддержка Razor pages
 builder.Services.AddRazorPages();
+
+//Примеры сервисов (Event и репозитория)
 builder.Services.AddTransient<IEventService, EventService>();
 builder.Services.AddTransient<IEventRepository, EventRepository>();
+
+//Пример добавение Automapper с кастомным профилем
 builder.Services.AddSingleton(new MapperConfiguration(mc =>
 {
     mc.AddProfile<ContractProfile>();
 }).CreateMapper());
+
+
 var app = builder.Build();
+
+//Пример метода Map без указания метода
 app.Map("/", async context =>
 {
     //может вернуть Null
@@ -29,8 +38,12 @@ app.Map("/", async context =>
     if (eventService != null) 
         await context.Response.WriteAsJsonAsync(await eventService.GetAllActiveEvents());
 });
+
+//Подключение в цепочку статических файлов
 app.UseStaticFiles();
 
+//Подключение анализ на сопоставление запроса с  RazorPage 
 app.MapRazorPages();
 
+//Запуск приложения для начала прослушивания узла на запросы
 app.Run();
